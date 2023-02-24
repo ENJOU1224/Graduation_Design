7bf40c0af78b131234e86df4e8a7f45cc39711b7
enjou <enjou1224@outlook.com>
2023-02-24 15:55:16 +0800

    两位饱和计数器感觉逻辑有问题，如果我现在这么做的话会用到超多饱和计数器的，改成全体就使用一个饱和计数器

--- work/SaturationgCounter/SaturationgCounter.v
+++ work/SaturationgCounter/SaturationgCounter.v
@@ -3,19 +3,28 @@ module SaturationgCounter (
 	input			clk,
 	input			rstn,
 
-	// 分支预测相关信号输入输出 
+	// 分支预测相关信号输出 
+	output		PredictResult,			// 分支预测结果	
+
+	// 饱和计数器更新相关
 	input			BranchTaken,			// 分支发生
-	input			BranchInst,				// 是否分支指令
-	output		PredictResult			// 分支预测结果	
+	input			BranchInst				// 是否分支指令
 );
 
 reg		[1:0] Counter; 
+wire	[1:0] NextValue;
 wire	[1:0] CounterNextValue;
-wire				Strongly	= ^Counter;
 
-assign CounterNextValue = {{2{~BranchInst}} & Counter}
-												| {{2{ BranchInst}}}
-// 状态机 
+CounterNextValueGenerator CounterNextValueGenerator1(
+	.CurrentValue				(Counter		),
+	.Taken							(BranchTaken),
+	.NextValue					(NextValue	)
+);
+
+assign CounterNextValue = {{2{~BranchInst}} & Counter		}						// 如果没有遇到分支指令，则维持现在的值
+												| {{2{ BranchInst}} & NextValue };					// 如果遇到分支指令，换用新值
+
+// 两位饱和寄存器，复位状态为01（weakly not taken） 
 always @(posedge clk or negedge rstn) begin
 	if (~rstn) begin
 		Counter <= 2'b01;	
