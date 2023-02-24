bc159e83c2a64ab9ab5b5740730c71754a366a13
enjou <enjou1224@outlook.com>
2023-02-24 17:06:17 +0800

    单个改造完成了，但是例化这么多有点怪，应该要做成grf那个形式，准备要这个成为模板改造了

--- work/SaturationgCounter/SaturationgCounter.v
+++ work/SaturationgCounter/SaturationgCounter.v
@@ -1,28 +1,21 @@
 module SaturationgCounter (
 	// 时钟信号及复位信号
-	input			clk,
-	input			rstn,
+	input				clk,
+	input				rstn,
 
 	// 分支预测相关信号输出 
-	output		PredictResult,			// 分支预测结果	
+	output[1:0]	PredictResult,		// 分支预测结果	
 
 	// 饱和计数器更新相关
-	input			BranchTaken,			// 分支发生
-	input			BranchInst				// 是否分支指令
+	input				CounterUpdate,		// 是否更新当前饱和计数器
+	input [1:0]	NextValue					// 传入的饱和计数器下一迭代值
 );
 
 reg		[1:0] Counter; 
-wire	[1:0] NextValue;
 wire	[1:0] CounterNextValue;
 
-CounterNextValueGenerator CounterNextValueGenerator1(
-	.CurrentValue				(Counter		),
-	.Taken							(BranchTaken),
-	.NextValue					(NextValue	)
-);
-
-assign CounterNextValue = {{2{~BranchInst}} & Counter		}						// 如果没有遇到分支指令，则维持现在的值
-												| {{2{ BranchInst}} & NextValue };					// 如果遇到分支指令，换用新值
+assign CounterNextValue = {{2{~CounterUpdate}} & Counter		}						// 如果没有遇到分支指令，则维持现在的值
+												| {{2{ CounterUpdate}} & NextValue };					// 如果遇到分支指令，换用新值
 
 // 两位饱和寄存器，复位状态为01（weakly not taken） 
 always @(posedge clk or negedge rstn) begin
@@ -34,7 +27,7 @@ always @(posedge clk or negedge rstn) begin
 	end	
 end
 
-assign PredictResult = Counter[1];
+assign PredictResult	= Counter;
 	
 
 endmodule
